ad75941cb49473bf89e33b8e7641c055e018154b
Yes98 <dtm17c@my.fsu.edu>
2020-07-19 19:37:52 -0400

    added skip button

--- SearchUm/app/src/main/java/com/mobile/searchum/searchScreen.java
+++ SearchUm/app/src/main/java/com/mobile/searchum/searchScreen.java
@@ -59,6 +59,7 @@ import android.os.Bundle;
 
 import android.util.Log;
 import android.util.Size;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.util.Strings;
@@ -107,6 +108,9 @@ public class searchScreen extends AppCompatActivity {
     private String current = null;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     int Frame = 0;
+
+    Button skip;
+
     @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -125,9 +129,20 @@ public class searchScreen extends AppCompatActivity {
         }
 
 
+        skip = findViewById(R.id.skip);
+        skip.setOnClickListener(skipListener);
         startCamera();
         startGame();
     }
+
+    private final View.OnClickListener skipListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            chooseObject();
+        }
+
+    };
+
 
     private void startGame(){
 
@@ -148,6 +163,11 @@ public class searchScreen extends AppCompatActivity {
             findAble.remove(choice);
             TextView c = (TextView) findViewById(R.id.current);
             c.setText(current);
+        }
+        else
+        {
+            TextView c = (TextView) findViewById(R.id.current);
+            c.setText("you found it all");
         }
     }
 
@@ -260,28 +280,7 @@ public class searchScreen extends AppCompatActivity {
                         .setTargetResolution(new Size(1280, 960))
                         .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
                         .build();
-        /*
-        OrientationEventListener orientationEventListener = new OrientationEventListener((Context)this) {
-            @Override
-            public void onOrientationChanged(int orientation) {
-                int rotation;
-                // Monitors orientation values to determine the target rotation value
-                if (orientation >= 45 && orientation < 135) {
-                    rotation = Surface.ROTATION_270;
-                } else if (orientation >= 135 && orientation < 225) {
-                    rotation = Surface.ROTATION_180;
-                } else if (orientation >= 225 && orientation < 315) {
-                    rotation = Surface.ROTATION_90;
-                } else {
-                    rotation = Surface.ROTATION_0;
-                }
 
-                imageAnalysis.setTargetRotation(rotation);
-            }
-        };
-
-        orientationEventListener.enable();
-*/
         imageAnalysis.setAnalyzer(getMainExecutor(),new ImageAnalysis.Analyzer() {
             @Override
             public void analyze(ImageProxy image) {
@@ -291,9 +290,8 @@ public class searchScreen extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "analyze", Toast.LENGTH_LONG);
                 Log.d("ANAL","in anal");
 
-                try {
                     // input 300x300x3
-                    Interpreter tflite = new Interpreter(loadModelFile(mCurrentActivity));
+                    //Interpreter tflite = new Interpreter(loadModelFile(mCurrentActivity));
                     Log.d("LOAD", "loaded");
                     Toast.makeText(getApplicationContext(), "hello", Toast.LENGTH_LONG);
                     // image to butmatp insprried by https://heartbeat.fritz.ai/image-classification-on-android-with-tensorflow-lite-and-camerax-4f72e8fdca79
@@ -347,178 +345,6 @@ public class searchScreen extends AppCompatActivity {
                                                     }
                                                 });
                     }
-
-
-                    /*
-                     @SuppressLint("UnsafeExperimentalUsageError")
-
-                    Image inputImage =image.getImage();
-                    if (inputImage != null) {
-                        InputImage inIm =
-                                InputImage.fromMediaImage(inputImage, image.getImageInfo().getRotationDegrees());
-                        ImageLabeler labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
-                        labeler.process(inIm)
-                                .addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
-                                    @Override
-                                    public void onSuccess(List<ImageLabel> labels) {
-                                        Log.d("Pass", String.valueOf(labels.get(0)));
-
-                                        // Task completed successfully
-                                        // ...
-                                    }
-                                })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Log.d("failure",e.getLocalizedMessage());
-                                        // Task failed with an exception
-                                        // ...
-                                    }
-                                });
-
-
-                    }
-
-
-                    /*
-
-                        ImageProxy.PlaneProxy y = image.getPlanes()[0];
-                    ImageProxy.PlaneProxy u = image.getPlanes()[1];
-                    ImageProxy.PlaneProxy v = image.getPlanes()[2];
-                    int ysize = y.getBuffer().remaining();
-                    int usize = u.getBuffer().remaining();
-                    int vsize = v.getBuffer().remaining();
-                    byte[] temp = new byte[ysize + usize + vsize];
-                    y.getBuffer().get(temp, 0, ysize);
-                    u.getBuffer().get(temp, ysize, vsize);
-                    v.getBuffer().get(temp, ysize + vsize, usize);
-
-                    YuvImage yuvImage = new YuvImage(temp, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
-                    ByteArrayOutputStream out = new ByteArrayOutputStream();
-                    yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 75, out);
-                    byte[] iBytes = out.toByteArray();
-                    int [] rgbBytes = new int[iBytes.length];
-                    //ImageUtils.convertYUV420ToARGB8888(y.getBuffer().array(),u.getBuffer().array(),v.getBuffer().array(),
-                    //        1280,960,y.getRowStride(),u.getRowStride(),u.getPixelStride(),rgbBytes);
-  //                  Bitmap rgbFrameBitmap = Bitmap.createBitmap(1280, 960, Bitmap.Config.ARGB_8888);
-//                    rgbFrameBitmap.setPixels(rgbBytes, 0, 1280, 0, 0, 1280, 960);
-
-                    Bitmap input = Bitmap.createScaledBitmap(BitmapFactory.decodeByteArray(iBytes, 0, iBytes.length), 300, 300, false);
-                    //Bitmap input = BitmapFactory.decodeByteArray(iBytes, 0, iBytes.length);
-                    //1280X960 image dimensions
-                    //Bitmap input = Bitmap.createScaledBitmap(rgbFrameBitmap,300,300,false);
-                    int wSize = input.getWidth();
-                    int hSize = input.getHeight();
-                    int[] intValues = new int[wSize * hSize];
-                    input.getPixels(intValues, 0, input.getWidth(), 0, 0, 300, 300);
-                    Log.d("data", String.valueOf(input.getConfig()));
-
-
-                    // for quantised model
-
-                    ByteBuffer imgData;
-                    imgData = ByteBuffer.allocateDirect(wSize * hSize * 3);
-                    //imgData.rewind();
-                    for (int i = 0; i < wSize; ++i) {
-                        for (int j = 0; j < hSize; ++j) {
-                            int pixelValue = intValues[i * wSize + j];
-                            imgData.put((byte) ((pixelValue >> 16) & 0xFF));
-                            imgData.put((byte) ((pixelValue >> 8) & 0xFF));
-                            imgData.put((byte) (pixelValue & 0xFF));
-                        }
-                    }
-
-                    Map<Integer, Object> output = new HashMap<>();
-
-                    // how many detections
-                    float[] numDetect = new float[1];
-                    float[][] outClasses = new float[1][10];
-                    float[][] outScores = new float[1][10];
-                    float[][][] loc = new float[1][10][4];
-
-                    output.put(3, numDetect);
-                    //classes
-                    output.put(1, outClasses);
-                    // scores
-                    output.put(2, outScores);
-                    //locations
-                    output.put(0, loc);
-
-                        Object[] tfliteInput = {imgData};
-                        tflite.runForMultipleInputsOutputs(tfliteInput, output);
-                        // Log.d("ic", output.get(3)));
-
-                        int numDetectionsOutput = Math.min(10, (int) numDetect[0]);
-                        //Log.d("found", String.valueOf(numDetect[0]));
-                        //Log.d("ic", String.valueOf(outClasses[0][0]));
-                        Vector<String> qLessClasses = new Vector();
-                        Vector percents = new Vector();
-                        for (int i = 0; i < outClasses[0].length; i++) {
-                            String cla = Objects[(int) outClasses[0][i]];
-                            if (!cla.equals("???")) {
-                                qLessClasses.add(cla);
-                                percents.add(outScores[0][i]);
-                            }
-                        }
-
-                        int[] top3 = findTop3(percents);
-                       // if(top3.length == 3) {
-                            for (int i = 0; i < qLessClasses.size(); i++) {
-                               Log.d("hceck loop",qLessClasses.get(i));
-                               Log.d("hceck loop",current);
-
-                                if (qLessClasses.get(i).equals(current)) {
-                                    chooseObject();
-                                    //return;
-                                    break;
-                                }
-                            }
-                        //}
-                        /*
-                        if (top3.length == 3) {
-                            /*
-                            TextView t1, t1p, t2, t2p, t3, t3p;
-                            t1 = (TextView) findViewById(R.id.top1);
-                            t1p = (TextView) findViewById(R.id.top1P);
-                            t2 = (TextView) findViewById(R.id.top2);
-                            t2p = (TextView) findViewById(R.id.top2P);
-                            t3 = (TextView) findViewById(R.id.top3);
-                            t3p = (TextView) findViewById(R.id.top3P);
-                            t1.setText(qLessClasses.get(top3[0]));
-                            t1p.setText(String.valueOf((float) percents.get(top3[0])*10));
-
-                            t2.setText(qLessClasses.get(top3[1]));
-                            t2p.setText(String.valueOf((float)percents.get(top3[1])*10));
-
-                            t3.setText(qLessClasses.get(top3[2]));
-                            t3p.setText(String.valueOf((float)percents.get(top3[2])*10));
-
-
-                            for(int i = 0; i < qLessClasses.size();i++)
-                            {
-                               // Log.d("classes",String.valueOf(i)+" :"+qLessClasses.get(i));
-                                //Log.d("classes",String.valueOf(i)+"PErcenets :"+String.valueOf(percents.get(i)));
-                            }
-                            Log.d("classes","end");
-                            Log.d("classes","end");
-                            Log.d("classes","end");
-                        }
-                    /*
-                    for (int i = 0; i < 3; i ++)
-                    {
-                        Log.d("top3",qLessClasses.get(i));
-                        Log.d("top3","percent: "+percents.get(i));
-                    }*/
-
-
-
-
-                    tflite.close();
-                } catch (IOException e) {
-                    Log.d("FAIL","faileded");
-                    Toast.makeText(getApplicationContext(), "no", Toast.LENGTH_LONG);
-                    e.printStackTrace();
-                }
 
 
 
